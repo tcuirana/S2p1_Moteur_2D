c93a0ee16a5e1f37c75fcd41a82d57c22ff07da0
tcuirana <tcuirana@gaming.tech>
2026-01-12 21:49:48 +0100

    Fin SetDirection()

--- AMovable.cpp
+++ AMovable.cpp
@@ -1,12 +1,18 @@
 #include "AMovable.h"
+#include <cmath>
 
 AMovable::AMovable(Vector2 direction, float speed): _direction(direction), _speed(speed) {}
 
 void AMovable::SetDirection(Vector2 new_direction)
 {
-	_direction = new_direction;
+	float distanceX = (_direction.GetX() - new_direction.GetX()) * (_direction.GetX() - new_direction.GetX());
+	float distanceY = (_direction.GetY() - new_direction.GetY()) * (_direction.GetY() - new_direction.GetY());
+	float distance = sqrt(distanceX + distanceY);
 
-	_direction = ;
+	new_direction.SetX(new_direction.GetX() / distance);
+	new_direction.SetY(new_direction.GetY() / distance);
+
+	_direction = new_direction ;
 }
 
 void AMovable::SetSpeed(float new_speed)
